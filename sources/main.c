cf5a5501cd334c325f8a268e8d5b2a33be8077e1
CharMe1001 <stefan.moldoveanu@s.unibuc.ro>
2022-11-18 09:22:27 +0300

    Daemonized program that uses header.

--- sources/main.c
+++ sources/main.c
@@ -1,7 +1,33 @@
 #include "hello.h"
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
 int main()
 {
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror(NULL);
+		return errno;
+	} else if (pid) {
+		return 0;
+	}
+	
+	setsid();
+	
+	pid = fork();
+	if (pid < 0) {
+		perror(NULL);
+		return errno;
+	} else if (pid) {
+		return 0;
+	}
+	
+	umask(0);
+	chdir("/");
+	
 	hello_1();
 	hello_2();
 
